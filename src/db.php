feebf9a1d497285168a497489c4cdf37512a08cd
Francisco Javier Usero Sanchez <usero.sanchez.f@gmail.com>
2025-04-17 21:11:21 +0200

    Si

--- src/db.php
+++ src/db.php
@@ -1,13 +1,21 @@
 <?php
 $host = 'db5017684907.hosting-data.io';
-$dbname = 'dbs14141935';
-$username = 'dbu319932';
-$password = 'penedestroyxz';
+$db   = 'dbs14141935';
+$user = 'dbu319932';
+$pass = 'penedestroyxz';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;port=3306;charset=$charset";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    die("Error de conexión: " . $e->getMessage());
+     $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+     throw new \PDOException("Error de conexión: " . $e->getMessage(), (int)$e->getCode());
 }
 ?>
