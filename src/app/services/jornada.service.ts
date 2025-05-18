53527b4df76fa54594e79e04736fefb46094dbd1
Francisco Javier Usero Sanchez <usero.sanchez.f@gmail.com>
2025-05-19 00:43:37 +0200

    s

--- src/app/services/jornada.service.ts
+++ src/app/services/jornada.service.ts
@@ -1,6 +1,4 @@
-
-
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
@@ -10,39 +8,50 @@ export class JornadaService {
 
   constructor(private http: HttpClient) {}
 
-   iniciarJornada(): Observable<any> {
-    return this.http.post(`${this.apiUrl}/iniciar`, {});
+  // Encapsulamos opciones con headers y withCredentials
+  private getOptions(): { headers: HttpHeaders; withCredentials: boolean } {
+    const token = localStorage.getItem('token');
+    return {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${token}`
+      }),
+      withCredentials: true
+    };
+  }
+
+  iniciarJornada(): Observable<any> {
+    return this.http.post(`${this.apiUrl}/iniciar`, {}, this.getOptions());
   }
 
   finalizarTramo(): Observable<any> {
-    return this.http.put(`${this.apiUrl}/finalizar`, {});
+    return this.http.put(`${this.apiUrl}/finalizar`, {}, this.getOptions());
   }
 
   obtenerJornadaActiva(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/activa`);
+    return this.http.get(`${this.apiUrl}/activa`, this.getOptions());
   }
 
   obtenerJornadaDeHoy(): Observable<any> {
-    return this.http.get(`${this.apiUrl}/hoy`);
+    return this.http.get(`${this.apiUrl}/hoy`, this.getOptions());
   }
 
   obtenerTodasLasJornadas(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/todas`);
+    return this.http.get<any[]>(`${this.apiUrl}/todas`, this.getOptions());
   }
 
   obtenerTrabajadores(): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/usuarios`);
+    return this.http.get<any[]>(`${this.apiUrl}/usuarios`, this.getOptions());
   }
 
   eliminarJornada(id: number): Observable<any> {
-    return this.http.delete(`${this.apiUrl}/${id}`);
+    return this.http.delete(`${this.apiUrl}/${id}`, this.getOptions());
   }
 
   obtenerTramosPorJornada(jornadaId: number): Observable<any[]> {
-    return this.http.get<any[]>(`${this.apiUrl}/${jornadaId}/tramos`);
+    return this.http.get<any[]>(`${this.apiUrl}/${jornadaId}/tramos`, this.getOptions());
   }
+
   actualizarTramos(payload: any): Observable<any> {
-    return this.http.put(`${this.apiUrl}/editar-tramos`, payload);
+    return this.http.put(`${this.apiUrl}/editar-tramos`, payload, this.getOptions());
   }
-  
 }
