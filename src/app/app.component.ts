406eb1fc1c33952ae6dd4cc4903e24d7d24fa7f4
Francisco Javier Usero Sanchez <159946050+Destroyxz@users.noreply.github.com>
2025-05-29 14:41:22 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+//Importamos los componentes que usaremos 
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from './services/auth.service';
@@ -8,19 +9,20 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  // Controla si el sidebar está expandido o colapsado
   expanded = false;
-
+  
   constructor(
     private authService: AuthService,
     public router: Router
   ) { }
 
+  // Cierra la sesión del usuario y limpia los datos
   onLogout(): void {
     this.authService.logout();
   }
 
-  // ✅ Devuelve true si NO estás en login o register
+  // Devuelve true si no estas en el login o en el registro, para ocultar el sidebar y header en el login x ejemplo
   shouldShowLayout(): boolean {
     return !['/login', '/register'].includes(this.router.url);
   }
