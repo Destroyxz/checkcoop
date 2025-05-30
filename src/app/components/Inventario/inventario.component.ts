4d25d309e7f4ec23369c35a56da5580a706576a8
Francisco Javier Usero Sanchez <159946050+Destroyxz@users.noreply.github.com>
2025-05-30 10:39:49 +0200

    Update inventario.component.ts

--- src/app/components/Inventario/inventario.component.ts
+++ src/app/components/Inventario/inventario.component.ts
@@ -1,3 +1,4 @@
+//Importamos los componentes necesarios
 import { Component, OnInit } from '@angular/core';
 import { ProductoService, Producto } from '../../services/producto.service';
 
@@ -11,18 +12,19 @@ export class InventarioComponent implements OnInit {
   filtroBusqueda: string = '';
   mostrarFormulario = false;
   mostrarModal: boolean = false;
-
+//Modelo de ejemplo de cuando creas un producto
   nuevoProducto: Producto = {
     numEmpresa: 1,
     nombre: '',
     descripcion: '',
     cantidad: 0,
-    unidad: 'unidad',
+    unidad: 'kg',
     categoria: '',
     precio: 0,
     imagen: '',
   };
 
+//Modelo de ejemplo de cuando actualizas un producto (para inicizalizar las variables si no los pilla bien)
   productoSeleccionado: Producto = {
     id: 0,
     numEmpresa: 1,
@@ -45,6 +47,8 @@ export class InventarioComponent implements OnInit {
   ngOnInit(): void {
     this.cargarProductos();
   }
+
+  //Esta funcion permite subir imagenes y limitar su formato
   onFileSelected(event: any, tipo: 'nuevo' | 'editar'): void {
     const file: File = event.target.files[0];
     if (!file) return;
@@ -74,31 +78,35 @@ export class InventarioComponent implements OnInit {
     };
     reader.readAsDataURL(file);
   }
-
+  
+//Funcion que obtiene los productos
   cargarProductos(): void {
     this.productoService.obtenerProductos().subscribe((data) => {
       this.productos = data;
     });
   }
 
+  //Filtra los productos segun su busqueda
   productosFiltrados(): Producto[] {
     return this.productos.filter((p) =>
       p.nombre.toLowerCase().includes(this.filtroBusqueda.toLowerCase())
     );
   }
 
+  //Establece el estado del producto segun su cantidad
   getEstadoTexto(p: Producto): string {
     if (p.cantidad === 0) return 'Agotado';
     if (p.cantidad <= 25) return 'Por Vencer';
     return 'Disponible';
   }
 
+  //Establece una clase o otra de bootstrap segun su cantidad
   getEstadoClase(p: Producto): string {
     if (p.cantidad === 0) return 'badge-danger';
     if (p.cantidad <= 25) return 'badge-warning text-dark';
     return 'badge-dark';
   }
-
+//Funcion que permite agregar productos
   agregarProducto(): void {
     const formData = new FormData();
     Object.entries(this.nuevoProducto).forEach(([key, value]) => {
@@ -111,13 +119,14 @@ export class InventarioComponent implements OnInit {
     this.productoService.agregarProducto(formData).subscribe({
       next: () => {
         alert('Producto creado correctamente');
+        //Reseteamos los valores a los por defecto
         this.mostrarFormulario = false;
         this.nuevoProducto = {
           numEmpresa: 1,
           nombre: '',
           descripcion: '',
           cantidad: 0,
-          unidad: 'unidad',
+          unidad: 'kg',
           categoria: '',
           precio: 0,
         };
@@ -132,15 +141,18 @@ export class InventarioComponent implements OnInit {
     });
   }
 
+  //Funcion que abre el modal de el producto seleccionado
   abrirModal(producto: Producto): void {
     this.productoSeleccionado = { ...producto };
     this.mostrarModal = true;
   }
 
+  //Funcion que permite cerrar el modal del producto
   cerrarModal(): void {
     this.mostrarModal = false;
   }
 
+  //Funcion que permite guardar los cambios del  producto 
   guardarCambios(): void {
     const formData = new FormData();
     Object.entries(this.productoSeleccionado).forEach(([key, value]) => {
@@ -170,6 +182,7 @@ export class InventarioComponent implements OnInit {
       });
   }
 
+  //Funcion que permite eliminar el producto seleccionado
   eliminarProducto(producto: Producto): void {
     const confirmar = confirm(
       `¿Seguro que deseas eliminar el producto "${producto.nombre}"?`
